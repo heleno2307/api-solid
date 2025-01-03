b57f08babb97e4edeb2779d3a343efc91c34547a
heleno <marioanimes09@gmail.com>
2025-01-03 16:44:44 -0300

    finalizando configurações

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
-import { app } from "./app";
+import { app } from './app'
+import { env } from './env'
 
-app.listen({
-   host: '0.0.0.0',
-   port: 3333
-}).then(()=> console.log('HTTP Server Runnig!'))
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => console.log(`HTTP Server Runnig! ${env.PORT}`))
