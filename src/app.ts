e08f431fafa5352f9b6d46f91faa2d1d2273c11f
heleno <marioanimes09@gmail.com>
2025-01-09 12:06:45 -0300

    add refresh token functionality with cookie support

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import fastify from 'fastify'
 import { usersRoutes } from './http/controllers/users/routes'
 import { ZodError } from 'zod'
 import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 import { env } from './env'
 import { gymsRoutes } from './http/controllers/gyms/routes'
 import { checkInsRoutes } from './http/controllers/check-ins/routes'
@@ -10,7 +11,11 @@ export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: { cookieName: 'refreshToken', signed: false },
+  sign: { expiresIn: '10m' },
 })
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,12 @@ import { register } from './register.controller'
 import { authenticate } from './authenticate.controller'
 import { profile } from './profile.controller'
 import { verifyJwt } from '../../middlewares/verify-jwt.middleware'
+import { refresh } from './refresh.controller'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+  app.patch('/token/refresh', refresh)
 
   app.get(
     '/me',
