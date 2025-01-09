d1638a42da4557fa303a0670a6525a92de45f175
heleno <marioanimes09@gmail.com>
2025-01-09 12:24:51 -0300

    implement user role management with JWT and middleware for admin access control

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -3,11 +3,12 @@ import { FastifyInstance } from 'fastify'
 import { search } from './search.controller'
 import { nearby } from './nearby.controller'
 import { create } from './create.controller'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role.middleware'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
   app.get('/gyms/search', search)
   app.get('/gyms/nearby', nearby)
 
-  app.post('/gyms', create)
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create)
 }
